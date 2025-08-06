b7d8771417ecc7424e3932f44c5fd68a424a9a8b
jammy0101 <walker0101walker.gmail.com>
2025-08-06 17:41:14 +0500

    counter problem solved . when i click on increment button then number will increment and vice versa.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
-import 'package:bloc_k/equatable_testing.dart';
+
+import 'package:bloc_k/bloc/counter/counter_bloc.dart';
+import 'package:bloc_k/ui/counter_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,14 +14,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    return BlocProvider(
+        create: (_) => CounterBloc(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        ),
+        home: CounterScreen(),
       ),
-      home: EquatableTesting(),
     );
   }
 }
